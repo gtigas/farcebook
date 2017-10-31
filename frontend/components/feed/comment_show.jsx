1698e1b4121d57626fee2155de88ef754bd9d629
Glenn Tigas <glenn.tigas@gmail.com>
2017-10-31 10:32:44 -0400

    Add liking to comments and styling

--- frontend/components/feed/comment_show.jsx
+++ frontend/components/feed/comment_show.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
+import { like, unlike } from '../../actions/likes_actions'
 import moment from 'moment';
 
 class CommentShow extends React.Component {
@@ -8,10 +9,19 @@ class CommentShow extends React.Component {
     super(props);
     this.state = { showX: false }
     this.handleHover = this.handleHover.bind(this)
+    this._toggleLike = this._toggleLike.bind(this)
   }
 
   handleHover(){
     this.setState( { showX: !this.state.showX })
+  }
+
+  _toggleLike(){
+    if (this.props.comment.currentUserLikes) {
+      this.props.unlike(this.props.comment.id)
+    } else {
+      this.props.like(this.props.comment.id)
+    }
   }
 
   render(){
@@ -34,9 +44,17 @@ class CommentShow extends React.Component {
           </p>
           <div className='comment-show-bottom flex-row'>
             <ul className='flex-row' id='comment-nav'>
-              <li>Like</li>
+              <li onClick={this._toggleLike}>
+                {comment.currentUserLikes ? 'Unlike' : 'Like'}
+              </li>
               <li>Reply</li>
             </ul>
+            {comment.liker_ids.length > 0 &&
+              <figure className='comments-likes-show flex-row'>
+                <i className="fa fa-thumbs-up" aria-hidden="true"></i>
+                <h5>{comment.liker_ids.length} ·</h5>
+              </figure>
+            }
             <a title={date.format("dddd, MMMM Do YYYY, h:mm:ss a")} id='comment-time'>
               <i>{date.fromNow(true)}</i>
             </a>
@@ -57,7 +75,8 @@ const mapStateToProps = (state, ownProps) => {
 };
 
 const mapDispatchToProps = dispatch => ({
-
+  like: commentId => dispatch(like('comments', commentId)),
+  unlike: commentId => dispatch(unlike('comments', commentId)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(CommentShow);
