04a4f731aa16c633302977320dacea73616e243a
Glenn Tigas <glenn.tigas@gmail.com>
2017-11-02 23:29:05 -0400

    Change pointer on post image

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,17 +1,18 @@
 import { createStore, applyMiddleware } from 'redux';
 import RootReducer from '../reducers/root_reducer';
 import thunk from 'redux-thunk';
+import { logger } from 'redux-logger'
 
-const middlewares = [thunk];
+// const middlewares = [thunk];
 
-if (process.env.NODE_ENV !== 'production') {
-  // must use 'require' (import only allowed at top of file)
-  const { logger } = require('redux-logger');
-  middlewares.push(logger);
-}
+// if (process.env.NODE_ENV !== 'production') {
+//   // must use 'require' (import only allowed at top of file)
+//   const { logger } = require('redux-logger');
+//   middlewares.push(logger);
+// }
 
 const configureStore = (preloadedState = {} ) => {
-  return createStore(RootReducer, preloadedState, applyMiddleware(...middlewares))
+  return createStore(RootReducer, preloadedState, applyMiddleware(thunk, logger))
 }
 
 export default configureStore;
